537a5eb58bff10301853db9850838aeacbd71345
lanryZhang <hookyzlr@163.com>
2012-10-28 13:15:25 +0800

    v1
    Signed-off-by: lanryZhang <hookyzlr@163.com>

---
+++ SimpleElance/Project/UI/ViewModels/UserInfoModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace UI.ViewModels
+{
+    public class UserInfoModel
+    {
+        public string UserID { get; set; }
+
+        [Display(Name = "FirstName", ResourceType = typeof(Internationalization.Resources))]
+        [Required(ErrorMessageResourceName = "FirstNameRequired", ErrorMessageResourceType = typeof(Internationalization.Resources))]
+        [StringLength(20, ErrorMessageResourceName = "UserNameMaxLength", ErrorMessageResourceType = typeof(Internationalization.Resources))]
+        public string FirstName { get; set; }
+
+        [Display(Name = "LastName", ResourceType = typeof(Internationalization.Resources))]
+        [Required(ErrorMessageResourceName = "LastNameRequired", ErrorMessageResourceType = typeof(Internationalization.Resources))]
+        [StringLength(20, ErrorMessageResourceName = "UserNameMaxLength", ErrorMessageResourceType = typeof(Internationalization.Resources))]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email", ResourceType = typeof(Internationalization.Resources))]
+        [Required(ErrorMessageResourceName = "EmailRequired", ErrorMessageResourceType = typeof(Internationalization.Resources))]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessageResourceName = "EmailFaield", ErrorMessageResourceType = typeof(Internationalization.Resources))]
+        public string Email { get; set; }
+
+        [Display(Name = "Country", ResourceType = typeof(Internationalization.Resources))]
+        public int CountryID { get; set; }
+
+        [Display(Name = "CompanyName", ResourceType = typeof(Internationalization.Resources))]
+        public string CompanyName { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Internationalization.Resources), ErrorMessageResourceName = "UserNameRequired")]
+        [Display(Name = "UserName", ResourceType = typeof(Internationalization.Resources))]
+        [StringLength(20, ErrorMessageResourceType = typeof(Internationalization.Resources), ErrorMessageResourceName = "UserNameMaxLength")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Internationalization.Resources), ErrorMessageResourceName = "PasswordRequired")]
+        [Display(Name = "PassWord", ResourceType = typeof(Internationalization.Resources))]
+        [DataType(DataType.Password)]
+        public string PassWord { get; set; }
+
+        [Display(Name = "RetypePassWord", ResourceType = typeof(Internationalization.Resources))]
+        [Required(ErrorMessageResourceType = typeof(Internationalization.Resources), ErrorMessageResourceName = "RetypePassWordRequired")]
+        [DataType(DataType.Password)]
+        [Compare("PassWord", ErrorMessageResourceType = typeof(Internationalization.Resources), ErrorMessageResourceName = "PassWordFaield")]
+        public string RetypePassword { get; set; }
+
+        [Display(Name = "How", ResourceType = typeof(Internationalization.Resources))]
+        public int How { get; set; }
+
+        [Display(Name = "AccountType", ResourceType = typeof(Internationalization.Resources))]
+        public int AccountType { get; set; }
+
+        [Display(Name = "DisplayName", ResourceType = typeof(Internationalization.Resources))]
+        public string DisplayName { get; set; }
+
+        public int Type { get; set; }
+    }
+}
